80915116d2656911b6e74de79e76d29937df0e69
Enes Sefa <bilgi@enessefa.com>
2017-08-26 09:28:08 +0300

    düzenlemeler

--- dev/assets/scripts/app.js
+++ dev/assets/scripts/app.js
@@ -1,15 +1,62 @@
-const choices = new Choices('.select', {
+new Choices('.select', {
   noResults: 'has-no-results',
   noChoices: 'has-no-choices',
   removeItems: true,
   removeItemButton: true,
 });
 
-document.getElementById("mobilenav-btn").addEventListener("click", function(){
+document.getElementById("mobilenav").addEventListener("click", function(){
+  const navMenu = document.getElementById("header-nav");
   if (this.classList.contains("active")){
     this.classList.remove("active");
+    navMenu.style.display = "none";
   }
   else{
     this.classList.add("active");
+    navMenu.style.display = "block";
   }
 });
+
+const asideBtn = document.querySelectorAll(".asidesearchbtn"),
+  asideSearchBar = document.getElementById("searchaside"),
+  asideSearchBarWidth = asideSearchBar.offsetWidth,
+  pageWrapper = document.getElementById("page-wrapper"),
+  body = document.getElementsByTagName("body")[0];
+
+TweenMax.set(asideSearchBar, {
+  x: -asideSearchBarWidth
+});
+
+for (let i = 0; i < asideBtn.length; i++) {
+
+  asideBtn[i].addEventListener("click", function () {
+    if (asideSearchBar.classList.contains("active")) {
+      asideSearchBar.classList.remove("active");
+      body.style.overflow = "auto";
+      TweenMax.to(asideSearchBar, .5, {
+        x: -asideSearchBarWidth,
+        ease: Power0.easeNone,
+        yoyo: true
+      });
+      TweenMax.to(pageWrapper, .5, {
+        x: 0,
+        ease: Power0.easeNone,
+        yoyo: true
+      });
+    } else {
+      asideSearchBar.classList.add("active");
+      body.style.overflow = "hidden";
+      TweenMax.to(asideSearchBar, .5, {
+        x: 0,
+        ease: Power0.easeNone,
+        yoyo: true
+      });
+      TweenMax.to(pageWrapper, .5, {
+        x: 300,
+        ease: Power0.easeNone,
+        yoyo: true
+      });
+    }
+  });
+
+}
