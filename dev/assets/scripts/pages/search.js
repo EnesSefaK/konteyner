bfac7ea07a11328f3813e5f18a24ca814a06eb86
EnesSefaK <enes.sefa.k@gmail.com>
2017-08-29 22:34:29 +0300

    düzenlemeler

--- dev/assets/scripts/pages/search.js
+++ dev/assets/scripts/pages/search.js
@@ -1,13 +1,15 @@
 var selectedCount = document.querySelectorAll(".selected").length,
 removeInfo = document.getElementById("remove-info"),
 selectedInfo = document.getElementById("selected-info");
+
 function selected(e, product){
   var classList = product.classList;
   classList.contains("selected") ? classList.remove("selected") : classList.add("selected");
   selectedCount = document.querySelectorAll(".selected").length;
   selectedCount == 0 ? removeInfo.style.display = "block" : removeInfo.style.display = "none";
-  selectedInfo.innerText = 'Number of Selected Containers '+ selectedCount;
+  selectedInfo.innerHTML = 'Number of Selected Containers '+ '<span>'+selectedCount+'</span>';
 }
+
 function containerRequest(e, form){
   if(selectedCount != 0){
     var name = form["name"].value;
